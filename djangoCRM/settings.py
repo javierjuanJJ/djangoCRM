45164440548ef017a8e0e3b351a0814967f021e9
Javier Juan Uceda <javierjuanuceda18@gmail.com>
2023-03-27 10:09:32 +0200

    Commit 2 mysql configuration

--- djangoCRM/settings.py
+++ djangoCRM/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'website'
 ]
 
 MIDDLEWARE = [
@@ -76,8 +77,12 @@ WSGI_APPLICATION = 'djangoCRM.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'elderco',
+        'USER': 'root',
+        'PASSWORD': 'my-secret-pw',
+        'HOST': '192.168.1.112',
+        'PORT': '3307',
     }
 }
 
