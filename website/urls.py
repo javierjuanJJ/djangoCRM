6b5cb8e46812cf8ac188874e452a96f09149ef81
Javier Juan Uceda <javierjuanuceda18@gmail.com>
2023-03-28 10:13:19 +0200

    Commit 4 Login Users

--- website/urls.py
+++ website/urls.py
@@ -1,8 +1,9 @@
-from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
 
 from website import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('login/', views.login_user, name='login'),
+    path('logout/', views.logout_user, name='logout'),
 ]
