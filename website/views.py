e2cc55673064b5ad6c6227b0b4e86d9c8d8d648d
Javier Juan Uceda <javierjuanuceda18@gmail.com>
2023-03-29 23:58:40 +0200

    Commit 11 delete records

--- website/views.py
+++ website/views.py
@@ -59,3 +59,14 @@ def customer_record(request, pk):
     else:
         messages.success(request, "You must be logged into view in this page")
         return redirect('home')
+
+
+def delete_record(request, pk):
+    if request.user.is_authenticated:
+        delete_it = Record.objects.get(id = pk)
+        delete_it.delete()
+        messages.success(request, 'Records deleted succesfully')
+        return redirect('home')
+    else:
+        messages.success(request, "You Must Be Logged In To Do That...")
+        return redirect('home')
